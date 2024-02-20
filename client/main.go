b361f8788ac70dc2e901d16319701c791cf7e150
Nina Barbakadze <barbakadzeninaa@gmail.com>
2024-02-20 16:31:37 +0100

    feat: works but ugly

--- client/main.go
+++ client/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
 type Pet struct {
-	Name   string
 	Health int
 	Img    int
+}
+
+type Resp struct {
+	IsHealthier bool `json:"is_healthier"`
+	Scale       int  `json:"scale"`
 }
 
 func main() {
@@ -16,20 +22,58 @@ func main() {
 	fs := http.FileServer(http.Dir("./assets"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
+	resp, err := http.Get("http://172.16.24.166:8000/health")
+	if err != nil {
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error reading response body: %s\n", err)
+		return
+	}
+
+	var respTia Resp
+	err = json.Unmarshal(body, &respTia)
+	if err != nil {
+		fmt.Println("Error parsing JSON:", err)
+		return
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// Get the pet name from the URL query parameter "name"
 		petName := r.URL.Query().Get("name")
 		if petName == "" {
-			petName = "nina" // Default name if none is provided
+			petName = "stu" // Default name if none is provided
 		}
 
-		pet := Pet{Name: petName, Health: 80, Img: 4} // Initialize pet with dynamic name
+		pet := Pet{Health: respTia.Scale, Img: respTia.Scale} // Initialize pet with dynamic name
+
+		if respTia.Scale == 1 {
+			pet.Health = 20
+		} else if respTia.Scale == 2 {
+			pet.Health = 40
+		} else if respTia.Scale == 3 {
+			pet.Health = 60
+		} else if respTia.Scale == 4 {
+			pet.Health = 80
+		} else if respTia.Scale == 5 {
+			pet.Health = 100
+		}
 
 		// Generate HTML with embedded pet name and health value
 		fmt.Fprintf(w, `
 	<!DOCTYPE html>
 	<html lang="en">
 	<head>
+	   <script>
+	   // Refresh the page after a delay of 3 seconds
+	   setTimeout(function(){
+		   location.reload();
+	   }, 30000); // 3000 milliseconds = 3 seconds 
+	   </script>
     	<meta charset="UTF-8">
     <style>
 			body, html {
@@ -60,21 +104,26 @@ func main() {
 				height: 100;
 			}
 			.pet-name {
+				margin-top: 100px;
 				font-size: 24px;
-				margin-bottom: 20px;
+			}
+			.btn {
+				margin: 10px;
 			}
 		</style>
 	</head>
 	<body>
-		<div class="pet-name">tiagochi: %s</div>
+		<div class="pet-name">stuiegotchi</div>
 		<img src="/assets/%d.png" alt="Pet Image" class="pet-image" />
 		<b>Health: %d%%</b>
 		<div id="health-bar-container">
 			<div id="health-bar"></div>
 		</div>
+		<button class="btn" onclick="window.location.href = '/?name=st'">Feed</button>
+	  </div>
 	</body>
 	</html>
-	`, pet.Health, pet.Name, pet.Img, pet.Health)
+	`, pet.Health, pet.Img, pet.Health)
 	})
 
 	http.ListenAndServe(":8080", nil)
